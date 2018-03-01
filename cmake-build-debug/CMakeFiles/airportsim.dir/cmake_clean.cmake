7852972e76e28260c3093b29b9771c4d484dbf4b
Sam <samieboyyeke@gmail.com>
2018-03-01 13:53:39 +0100

    First push

---
+++ cmake-build-debug/CMakeFiles/airportsim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/airportsim.dir/main.cpp.o"
+  "airportsim.pdb"
+  "airportsim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/airportsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
