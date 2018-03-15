43d4356470718e35c0372d332fb8b0bf836c0035
Sam Legrand <sam.legrand@student.uantwerpen.be>
2018-03-15 17:02:39 +0100

    First test implementation + minor fixes

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,3 +2,30 @@
 // Created by sam on 15.03.18.
 //
 
+#include <iostream>
+#include <gtest/gtest.h>
+#include "airplane.h"
+#include "airport.h"
+#include "runway.h"
+
+class AirportSimTest: public ::testing::Test {
+protected:
+    virtual void SetUp(){
+
+    }
+    virtual void TearDown(){
+
+    }
+    Airplane airplane_;
+    Airport airport_;
+    Runway runway_;
+};
+
+TEST_F(AirportSimTest, TakeOff){
+    
+}
+
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
